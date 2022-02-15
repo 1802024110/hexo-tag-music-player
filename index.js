c06b754dba1e96f3d2afef352c06a3d5184bfb24
NoDream <1802024110@qq.com>
2022-02-15 15:32:05 +0800

    成功配置反代理

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 /* global hexo */
 'use strict';
 const path = require('path');
-// 为list模式的处理函数
+const { createProxyMiddleware } = require('http-proxy-middleware');
+/* 
+  反代理
+*/
+const apiProxy = createProxyMiddleware('/api', {
+  target: 'http://music.163.com/api/',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api/': '/', // rewrite path 将链接中的 /api/ 替换为 '/'
+  },
+});
+hexo.extend.filter.register('server_middleware', function (app) {
+  // 表示以 api 开头的请求将被转发
+  app.use('/api', apiProxy);
+});
+
+/* 
+  hexo-tag
+*/
 const listMode = require('./util/list-mode');
 hexo.extend.tag.register('musicPlay', function (args) {
   const source_dir = hexo.source_dir;
